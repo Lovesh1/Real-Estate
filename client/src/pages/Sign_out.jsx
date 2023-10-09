8b61eaab48ccc572b6c7bdf4856208fde7c7809d
Lovesh1 <lovesh688@gmail.com>
2023-10-09 14:42:03 +0530

    Add full fuctionality for signup page

--- client/src/pages/Sign_out.jsx
+++ client/src/pages/Sign_out.jsx
@@ -1,20 +1,59 @@
-import {Link} from 'react-router-dom'
+import { useState } from 'react'
+import {Link, useNavigate} from 'react-router-dom'
 
 export default function Sign_out() {
+
+    const [formdata, setformdata] = useState({})
+    const [error, seterror] = useState(null)
+    const [loading, setloading] = useState(false)
+    const navigate = useNavigate();
+    const handlechange = (e) => {
+      setformdata({
+        ...formdata,
+        [e.target.id]: e.target.value
+      });
+    };
+
+    const handlesubmit = async (e) => {
+      e.preventDefault();
+      setloading(true)
+      const res = await fetch("/api/auth/signup", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formdata),
+      });
+      const data = await res.json();
+  
+      if(data.success == false){
+        seterror(data.message);
+        setloading(false);
+        return
+      }
+      setloading(false);
+      navigate('/sign-in');
+      console.log(data);
+    };
+
   return (
     <div className="max-w-lg mx-auto p-3">
         <h1 className="text-3xl font-semibold text-center my-4">Sign Up</h1>
 
-          <form className="flex flex-col gap-3">
-              <input type="text" placeholder="Username" className="p-3 rounded-xl border border-slate-600 outline-none" id="username"></input>
-              <input type="email" placeholder="Email" className="p-3 rounded-xl border border-slate-600 outline-none" id="email"></input>
-              <input type="password" placeholder="Password" className="p-3 rounded-xl border border-slate-600 outline-none" id="password"></input>
-              <button className='bg-slate-600 text-white p-3 rounded-xl hover:opacity-90 disabled:opacity-80'>Sign Up </button>
+          <form className="flex flex-col gap-3" onSubmit={handlesubmit}>
+              <input type="text" placeholder="Username" className="p-3 rounded-xl border border-slate-600 outline-none" id="username" onChange={handlechange}></input>
+              <input type="email" placeholder="Email" className="p-3 rounded-xl border border-slate-600 outline-none" id="email" onChange={handlechange}></input>
+              <input type="password" placeholder="Password" className="p-3 rounded-xl border border-slate-600 outline-none" id="password" onChange={handlechange}></input>
+              <button disabled={loading} className='bg-slate-600 text-white p-3 rounded-xl hover:opacity-90 disabled:opacity-80'>{loading ? 'Loading..': 'Sign Up'} </button>
           </form>
             <div className='mt-3 flex gap-2'>
                 <p className='text-slate-400 font-thin'>Already have an Account?</p>
                 <Link to='/sign-in'><p className='text-blue-500'>Sign in</p></Link>
+
             </div>
+            {
+              error && <p className='text-xl font-bold text-red-400'>{error}</p>
+            }
     </div>
   )
 }
